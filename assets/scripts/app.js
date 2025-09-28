da43da9021ae874468baf2d3173b7cbb6531806d
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-28 15:02:44 +0700

    feat: apply controller to main.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,9 @@
 requirejs.config({
   baseUrl: "assets/scripts",
   paths: {
+    localStorageController: "./lib/localStorageController",
+    tasksController: "./lib/tasksController",
+    btnController: "./lib/btnController",
     jquery: "//cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min",
   },
 });
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,62 +1,14 @@
-define(["jquery"], function ($) {
+define(["jquery", "localStorageController", "tasksController", "btnController"], function ($, { setDataToLocalStorage, getDataTasks }, { processDataAdd }, btnController) {
+  btnController();
   $(() => {
-    $("#btn-dropdown-profile").on("click", function () {
-      $(this).children(".icon-dropdown").toggleClass("rotate-180 bg-[var(--primary)] bg-[var(--icon-second-color)]");
-    });
+    $("#form-add-task").submit(function (e) {
+      e.preventDefault();
+      const formData = $(this).serializeArray();
+      const processedData = processDataAdd(formData);
+      const dataTasks = getDataTasks();
+      setDataToLocalStorage(dataTasks, processedData);
 
-    $("#hamburger-menu").on("click", function () {
-      $("#nav-mobile").toggleClass("hidden");
-    });
-
-    $("#btn-add-item").on("click", function () {
-      $("#form-add-item").toggleClass("hidden");
-      $(this).children(".");
-    });
-
-    $("#btn-sorting-item").on("click", function () {
-      $("#option-sort").toggleClass("hidden");
-      $(this).toggleClass("text-[var(--primary)] border-[var(--primary)] border-[var(--icon-second-color)] text-[var(--text-secondary)]");
-      $(this).children(".icon-dropdown").toggleClass("rotate-180 bg-[var(--primary)] bg-[var(--icon-second-color)]");
-    });
-
-    $(".btn-dropdown-item").on("click", function () {
-      const idItem = $(this).data("idItem");
-      $(`#subtask-${idItem}`).toggleClass("hidden");
-      $(this).children(".icon-dropdown").toggleClass("rotate-180 bg-[var(--primary)] bg-[var(--icon-second-color)]");
-    });
-
-    $("#btn-completed-task").on("click", function () {
-      $("#completed-task").toggleClass("hidden");
-      $(this).children(".icon-dropdown").toggleClass("-rotate-180 bg-[var(--primary)] bg-[var(--icon-second-color)]");
-    });
-
-    $(".item-task").hover(function () {
-      const idItem = $(this).data("idItem");
-      $(`#btn-action-${idItem}`)
-        .toggleClass("hidden")
-        .on("click", function (e) {
-          e.stopPropagation();
-          $(`#option-action-${idItem}`)
-            .toggleClass("hidden")
-            .on("mouseleave", function () {
-              $(`#option-action-${idItem}`).addClass("hidden");
-            });
-        });
-    });
-
-    $(".checkbox-item").on("click", function () {
-      const idItem = $(this).data("idItem");
-      $(`#checkbox-${idItem}`).toggleClass("bg-[var(--primary)] bg-[url('./assets/img/check.png')] bg-no-repeat bg-center");
-    });
-
-    $(".checkbox-subtask").on("click", function () {
-      const idSubtask = $(this).data("idSubtask");
-      $(`#checkbox-${idSubtask}`).toggleClass("bg-[var(--primary)] bg-[url('./assets/img/check.png')] bg-no-repeat bg-center");
-      $(this).children("span").toggleClass("line-through");
-    });
-
-    $(".btn-completed-task").on("click", function () {
-      $(this).children(".icon-dropdown").toggleClass("rotate-180 bg-[var(--primary)] bg-[var(--icon-second-color)]");
+      $(this)[0].reset();
     });
   });
 });
